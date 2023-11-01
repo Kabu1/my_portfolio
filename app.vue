0956bf72e425218307c12f643560b46ad07fd38e
kabu1 <bensonmk99@gmail.com>
2023-11-01 18:01:31 -0400

    finalize project setup and basic home page

--- app.vue
+++ app.vue
@@ -1,5 +1,9 @@
 <template>
   <div>
-    hello nuxt 3
+    <SiteHeader/>
+    <main class="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+      <NuxtPage/>
+    </main>
   </div>
+  <SiteFooter/>
 </template>
---
+++ pages/index.vue
@@ -0,0 +1,34 @@
+<script>
+export default {
+
+}
+</script>
+<template>
+<section>
+    <h1 class="text-5xl font-bold mt-20"> 👋 Hi there, I'm Benson Mboci!</h1>
+    <p class="text-base text-gray-900 p-2 italic">Vuejs Developer at Home</p>
+</section>
+<section class="flex flex-col md:flex-row">
+    <div class="md:w-3/4">
+        <h2 class="text-3xl font-bold mt-8">My Story</h2>
+        <p class="text-lg py-2">
+            Played a pivotal role in the analysis and planning phase, working
+            closely with team members to assess the feasibility, benefits,
+            and challenges of adopting the new technology stack.
+        </p>
+        <p class="text-lg py-2">
+            Played a pivotal role in the analysis and planning phase, working
+            closely with team members to assess the feasibility, benefits,
+            and challenges of adopting the new technology stack.
+        </p>
+    </div>
+        <img src="~/assets/img/blackavatar.jpeg" alt="myavatar" class="w-1/2 md:max-w-sm p-8 mx-auto">
+</section>
+<section>
+<h2 class="text-3xl font-bold mt-8"> Latest Blog Post</h2>
+</section>
+</template>
+
+<style>
+
+</style>
