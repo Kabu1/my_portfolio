fcfdc828d5cde954efe6eba31e502dd05c33550c
kabu1 <bensonmk99@gmail.com>
2023-11-16 18:37:44 -0500

    list all the blog posts

--- pages/blog/index.vue
+++ pages/blog/index.vue
@@ -1,4 +1,4 @@
-<script>
+<script setup>
 const {
     data: posts
 } = await useAsyncData('posts', () =>
@@ -10,7 +10,12 @@ const {
 <h1 class="text-3xl my-8">Blog Posts</h1>
 Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis omnis odio, dolorem sunt ipsa facilis. Dignissimos ratione qui accusamus, optio vitae nisi magni porro quos perferendis voluptatem, temporibus, expedita modi?
 
-<section class="grid md:grid-cols-3 mt-8 gap-10">
-    <Post :posts="posts" />
-</section>
+<ul>
+    <li v-for="post in posts" :key="post._id" class="my-8 py-4 border-b-2">
+        <nuxt-link :to="post_path" :key="post._id" class="text-blue-500 text-2xl"> {{ post.title }}
+
+        </nuxt-link>
+        <p class="text-gray-500"> {{ post.description }}</p>
+    </li>
+</ul>
 </template>
